bfb38cecf0866158541a809756e4fb333e28a97f
bikramgiri <asgbrg.github@gmail.com>
2025-02-04 05:24:56 +0545

    add fetchproducts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,45 @@
+import { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar/Navbar'
 import './Home.css'
+import axios from 'axios'
 
 const Home = () => {
+  const [products, setProducts] = useState([])
+  const fetchProducts = async () => {
+    const response = await axios.get("https://67a13af45bcfff4fabe1ff80.mockapi.io/products")
+    setProducts(response.data)    
+}
+
+
+  useEffect(() => {   //  first time the component is rendered
+    fetchProducts()
+  },[])
   return (
-// This(<> </>) is a React fragment. It allows us to return multiple elements without adding an extra div.
-  <> 
+      <> 
       <Navbar />
-      <div className="card">
+      {
+        products.map((product)=>{
+          return (
+            <div key={product.id} className="card">
+              <img src={product.productImage} alt="Product Image" />
+              <h2 className="product-name">{product.productName}</h2>
+              <p className="product-description">{product.productDescription}</p>
+            </div>
+          )
+        })
+      }
+
+
+
+      {/* <div className="card">
       <img src="https://via.placeholder.com/300x200" alt="Product Image" className="card-image" />
       <div className="card-content">
           <h2 className="product-name">Product Name</h2>
           <p className="product-description">This is a brief description of the product. It can include details about its features, benefits, or any other relevant information.</p>
       </div>
-  </div>
-  </>
+      </div> */}
+
+      </>
   )
 }
 
